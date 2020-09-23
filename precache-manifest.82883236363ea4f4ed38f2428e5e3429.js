1ea383fce967e7489fe54c2ecbc6f845c187c18c
dannash100 <danster15@gmail.com>
2020-09-23 12:58:24 +1200

    Updates

---
+++ precache-manifest.82883236363ea4f4ed38f2428e5e3429.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80c830e63c0fc4a9cf0e4a4c8d88686",
+    "url": "/megs-project/index.html"
+  },
+  {
+    "revision": "9d753e2e3f6606c427f3",
+    "url": "/megs-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aba19ae6c3bacbdc9f94",
+    "url": "/megs-project/static/js/2.0845e138.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/megs-project/static/js/2.0845e138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d753e2e3f6606c427f3",
+    "url": "/megs-project/static/js/main.00083c24.chunk.js"
+  },
+  {
+    "revision": "51bf0f65af4f5c86baae",
+    "url": "/megs-project/static/js/runtime-main.986625c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/megs-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
