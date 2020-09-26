ae8bfaca08c26ee357e5908bbb6d48b06db15ee0
dannash100 <danster15@gmail.com>
2020-09-27 09:54:47 +1300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,95 +1,133 @@
-// import React, { useState, createContext } from "react";
-// import "./App.css";
-// import CrossfadeImage from "./CrossfadeImage";
+import React, { useState, useEffect } from "react";
+import CrossfadeImage from "react-crossfade-image";
+import { fixedShuffleIndex } from "./utils";
 
-// import logo from "./images/cnz_logo_png_white.png";
-// import livestreams from "./livestreams";
+import logo from "./images/cnz_logo_png_white.png";
+import imageData from "./images";
 
-// /**
-//  * <iframe src="https://g3.ipcamlive.com/player/player.php?alias=5f6417bdb19d7" width="800px" height="450px" 
-// frameborder="0" allowfullscreen></iframe>
+import "./App.css";
 
-// <iframe src="https://g3.ipcamlive.com/player/player.php?alias=5f685b9105a1d" width="800px" height="450px" 
-// frameborder="0" allowfullscreen></iframe>
+/** Start Config */
 
-// <iframe src="https://g3.ipcamlive.com/player/player.php?alias=5f685bd3cf2c3" width="800px" height="450px" 
-// frameborder="0" allowfullscreen></iframe>
+// Crossfade settings
+const crossfadeTimingFunction = "cubic-bezier(0, 0.55, 0.45, 1)";
+const crossfadeDuration = 900;
+const crossfadeStyles = { width: "100%", height: "100%" };
 
-// Parameters:
-// - alias: alias of your camera
-// - skin: orange/white (orange is the default)
-// - autoplay=1: start video automatically (this parameter works on desktop computers only)
-// - mute=1: mute the audio 
-// - disablefullscreen=1: disable full screen button
-// - disablevideofit=1: disable video fit button
-// - timelapseplayerenabled=1: enable the Time-lapse Hub on the player
-// - disabledownloadbutton=1: disable time-lapse download button in time-lapse mode
+// Variations in which images will crossfade on interval
+const patternVariations = 30;
 
-// Don't forget to adjust the height of the IFRAME according to the ASPECT RATIO of your camera! 
-//  */
+// Livestream
+const livestreamPositions = [
+  { x: 2, y: 1 },
+  { x: 0, y: 2 },
+  { x: 3, y: 3 },
+];
 
-// export const appContext = createContext(null);
+// Update time
+const minDelay = 3;
+const maxDelay = 6;
+/** End Config */
 
-// export const crossfadeInterval = 8000;
-// const delayOptions = 4;
+const livestreamCodes = ["5f6417bdb19d7", "5f685b9105a1d", "5f685bd3cf2c3"];
 
-// const delays = [...Array(delayOptions).keys()].map(
-//   (i) => (crossfadeInterval / delayOptions) * (i + 1)
-// );
+const Crossfade = ({ src }) => (
+  <CrossfadeImage
+    style={crossfadeStyles}
+    timingFunction={crossfadeTimingFunction}
+    duration={crossfadeDuration}
+    src={src}
+  />
+);
 
-// const livestreamPositions = [
-//   { x: 1, y: 3 },
-//   { x: 2, y: 1 },
-//   { x: 3, y: 2 },
-// ];
+const Livestream = ({ code }) => (
+  <div>
+    <iframe
+      key={code}
+      title={code}
+      src={`https://g3.ipcamlive.com/player/player.php?alias=${code}&autoplay=1&skin=white`}
+      width="auto"
+      height="100%"
+      style={{ height: "calc(100% + 1px)" }}
+      frameBorder="0"
+    />
+  </div>
+);
 
-// const randomDelay = () => delays[Math.floor(Math.random() * delays.length)];
+const Section = ({ src, pos }) => {
+  const { x, y } = pos;
+  const livestreamIndex = livestreamPositions.findIndex(
+    (lsPos) => lsPos.x == x && lsPos.y == y
+  );
+  if (livestreamIndex === -1) {
+    return <Crossfade src={src} />;
+  } else {
+    return <Livestream code={livestreamCodes[livestreamIndex]} />;
+  }
+};
 
-// const get4x4Matrix = () => {
-//   const matrix = [];
-//   let count = 0;
-//   for (var i = 0; i < 4; i++) {
-//     matrix[i] = [];
-//     for (var j = 0; j < 4; j++) {
-//       const liveStreamIndex = livestreamPositions.findIndex(
-//         (pos) => pos.x === j && pos.y === i
-//       );
-//       if (liveStreamIndex === -1) {
-//         matrix[i][j] = (
-//           <CrossfadeImage on delay={randomDelay()} startIdx={count % 14} />
-//         );
-//         count++;
-//       } else {
-//         matrix[i][j] = livestreams[liveStreamIndex];
-//       }
-//     }
-//   }
-//   return matrix;
-// };
+const lists = [...Array(patternVariations).keys()].map(() =>
+  [...Array(imageData.length).keys()].map(() => Math.random() >= 0.5)
+);
 
-// function App() {
-//   const [matrix, setMatrix] = useState(get4x4Matrix());
-//   const [visible, setVisible] = useState([]);
+let noChangeList = lists[0];
 
-//   console.log(visible);
-//   return (
-//     <div className="App">
-//       <div id="container">
-//         {matrix.map((row, i) => (
-//           <div key={`${row}-${i}`} className="row">
-//             {row.map((el, j) => (
-//               <div key={`box-${i}-${j}`} className="box">
-//                 <appContext.Provider value={{ visible, setVisible }}>
-//                   {el}
-//                 </appContext.Provider>
-//               </div>
-//             ))}
-//           </div>
-//         ))}
-//       </div>
-//       <img className="logo" src={logo} />
-//     </div>
-//   );
-// }
+function App() {
+  const [images, setImages] = useState(imageData);
 
-// export default App;
+  const update = () => {
+    const random = Math.round(
+      Math.random() * Math.abs(maxDelay - minDelay) + minDelay
+    );
+    setTimeout(function () {
+      setImages((oldVal) => fixedShuffleIndex(oldVal, noChangeList));
+      update();
+    }, random * 1000);
+  };
+
+  useEffect(update, []);
+
+  useEffect(
+    () =>
+      void setInterval(
+        () => (noChangeList = lists[Math.floor(Math.random() * lists.length)]),
+        2000
+      ),
+    []
+  );
+
+  return (
+    <div className="App">
+      <div id="container">
+        <div className="row">
+          <Section pos={{ x: 0, y: 0 }} src={images[0]} />
+          <Section pos={{ x: 1, y: 0 }} src={images[1]} />
+          <Section pos={{ x: 2, y: 0 }} src={images[2]} />
+          <Section pos={{ x: 3, y: 0 }} src={images[3]} />
+        </div>
+        <div className="row">
+          <Section pos={{ x: 0, y: 1 }} src={images[4]} />
+          <Section pos={{ x: 1, y: 1 }} src={images[5]} />
+          <Section pos={{ x: 2, y: 1 }} src={images[6]} />
+          <Section pos={{ x: 3, y: 1 }} src={images[7]} />
+        </div>
+        <div className="row">
+          <Section pos={{ x: 0, y: 2 }} src={images[8]} />
+          <Section pos={{ x: 1, y: 2 }} src={images[9]} />
+          <Section pos={{ x: 2, y: 2 }} src={images[10]} />
+          <Section pos={{ x: 3, y: 2 }} src={images[11]} />
+        </div>
+        <div className="row">
+          <Section pos={{ x: 0, y: 3 }} src={images[12]} />
+          <Section pos={{ x: 1, y: 3 }} src={images[13]} />
+          <Section pos={{ x: 2, y: 3 }} src={images[14]} />
+          <Section pos={{ x: 3, y: 3 }} src={images[1]} />
+        </div>
+      </div>
+
+      <img className="logo" src={logo} />
+    </div>
+  );
+}
+
+export default App;
